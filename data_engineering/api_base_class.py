48f8fd206cce0e1fd66c725a84a4ec01569a105a
herogreen <heiko-menzler@web.de>
2020-09-18 12:17:53 +0200

    Added simple account compatibility features.

--- data_engineering/api_base_class.py
+++ data_engineering/api_base_class.py
@@ -7,9 +7,15 @@ from datetime import datetime
 
 class APIBaseClass(ABC):
     """Abstract Base Class that implements an data gathering interface ."""
-    def __init__():
-        ...
+    def __init__(self,
+                 api_key: str = None,
+                 creds: dict = {
+                     'name': None,
+                     'pass': None
+                 }):
+        self.api_key = api_key
+        self.creds = creds
 
     @abstractmethod
-    def request_stock_data(name: str, start: datetime, end: datetime):
+    def request_stock_data(self, name: str, start: datetime, end: datetime):
         ...
